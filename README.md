03940a97dc254897c90c4c49b0540fb5346949a0
Sukkrivaa <sukkrivaa.vijayan@gmail.com>
2018-10-21 06:03:14 +0800

    Added REANDME and webpack.config.js

---
+++ README.md
@@ -0,0 +1,3 @@
+Open source this code only if you cannot find any funds at all to continue - then make it non-profit
+
+Make sure you test running this on another computer after finishing the final version - dev dependencies and dependencies are mixed up
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+	//Webapack Configuration
+	mode: "development",
+	entry: "./app/index.jsx",
+	output: {
+		filename: "./bundle.js",
+		publicPath:"/",
+	},
+	devtool: "eval-source-map",
+	module: {
+		rules: [
+			{
+				test: /\.(jsx?)$/,
+				exclude: /node_modules/,
+				loader: [{
+					loader: "babel-loader",
+					options: {
+						presets: ["react"]
+					}
+				}]
+			},
+			{
+				test: /\.scss$/,
+				use: [{
+					loader: "style-loader"
+				},
+				{
+					loader: "css-loader",
+					options: {
+						modules: true,
+						camelCase: true,
+						sourceMap: true
+					}
+				},
+				{
+					loader:"sass-loader"
+				}]
+			}
+		]
+	}
+};
